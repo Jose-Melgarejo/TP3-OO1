3bd7a9beef71c00c7e14e14e17280185cab8944f
Agustin Padron <agustinpadron94@gmail.com>
2016-09-06 16:54:14 -0300

    Se implemento la clase de prueba de Listas TestListas.

---
+++ src/listas/TestListas.java
@@ -0,0 +1,38 @@
+package listas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestListas {
+	public static void main(String argv[]) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Vamos a llenar un ArrayList");
+		
+		List<String> lista = new ArrayList<String>();
+		String elem = "";
+		
+		while(!(elem.equalsIgnoreCase("stop"))){
+			System.out.println("Para finalizar tipear stop");
+			elem = in.next();
+			if (!(elem.equalsIgnoreCase("stop"))) lista.add(elem);
+		}
+		
+		System.out.println("1) Impresion usando Foreach loop.");
+		for (String s : lista) {
+			System.out.println(s);
+		}
+		
+		System.out.println("2) Impresion usando for loop with index");
+		for (int p = 0; p < lista.size(); p++) {
+			System.out.println(lista.get(p));
+		}
+		
+		System.out.println("3) Impresion usando Iteration<tipo>");
+		for (Iterator<String> iter = lista.iterator(); iter.hasNext();) {
+			System.out.println(iter.next());
+		}
+	}
+}
