8133113970205dfcb9a4da38c997df339a08022e
jose melgarejo <josse_melgarejo@hotmail.com>
2016-09-20 10:58:27 -0300

    se agrego TestIncaa

--- src/test/TestIncaa.java
+++ src/test/TestIncaa.java
@@ -1,10 +1,28 @@
-package listas;
+package test;
 import incaa.*;
 
 public class TestIncaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Incaa incaa = new Incaa();
+		Genero genero = new Genero(1,"Terror");
+		Genero genero2 = new Genero(2,"Accion");
+		Genero genero3 = new Genero(3,"Comedia");
+		Genero genero4 = new Genero(4,"Drama");
+		Genero genero5 = new Genero(5,"Ficcion");
+		try{
+			incaa.agregarPelicula("Chuky", genero);	
+			incaa.agregarPelicula("Destino final", genero);	
+			incaa.agregarPelicula("Duro de matar", genero2);	
+			incaa.agregarPelicula("Scary Movie", genero3);	
+			incaa.agregarPelicula("pelicula 1", genero4);
+			incaa.agregarPelicula("I Robot", genero5);	
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		System.out.println(incaa.traerPelicula(1));
+		
 
 	}
 
